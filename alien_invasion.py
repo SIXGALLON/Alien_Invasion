167f8f325f1ef7b3b64900b5995224f02f13fa61
Sixgallon <1042368792@qq.com>
2022-02-11 00:59:12 +0800

    2022//2/11

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,9 @@
+from pickle import FALSE
 import sys
+from tkinter import RIGHT
 import pygame
+from settings import Settings
+from ship import Ship
 class AlienInvasion:
 
     """管理游戏资源和行为的类"""
@@ -7,28 +11,52 @@ class AlienInvasion:
         
         """初始化游戏并创建游戏资源"""
         pygame.init()
-        self.screen=pygame.display.set_mode((1200,800))
+        self.settings=Settings()
+        self.screen=pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien_invasion")
-        
-        #设置背景色
-        self.bg_color=(230,230,230)
+        self.ship=Ship(self)
 
     def run_game(self):
         
         """开始游戏的主循环"""
         while True:
+            self._check_event()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_event(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self,event):
+        """响应按键"""
+        if event.key==pygame.K_RIGHT:
+            self.ship.moving_right=True
+        elif event.key==pygame.K_LEFT:
+            self.ship.moving_left=True
+        elif event.key==pygame.K_q:
+            sys.exit()
             
-            """检测键盘和鼠标事件"""
-            for event in pygame.event.get():
-                if event.type==pygame.QUIT:
-                    sys.exit()
-                    
-            #每次循环时都重绘屏幕
-            self.screen.fill(self.bg_color)
-            
-            #让最近绘制的屏幕可见
-            pygame.display.flip()
-            
+    def _check_keyup_events(self,event):
+        """响应松开"""
+        if event.key==pygame.K_RIGHT:
+            self.ship.moving_right=False
+        elif event.key==pygame.K_LEFT:
+            self.ship.moving_left=False  
+
+    def _update_screen(self):
+        """更换屏幕上的新的图像,并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.bilitme()
+        
+        pygame.display.flip()                
+
 if __name__=="__main__":
     
     """创建游戏实例并运行游戏"""
